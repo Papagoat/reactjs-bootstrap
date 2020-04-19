22f315d4aa49133b4274915af7b4311a39f9e01d
Papagoat <terencelucasyap@gmail.com>
2020-04-19 22:18:40 +0800

    Removed unused modules

--- src/components/Header/Navlinks/Navlinks.js
+++ src/components/Header/Navlinks/Navlinks.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './Navlinks.css';
 
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import { BrowserRouter as Link } from 'react-router-dom';
 
 function Navlinks(props) {
-    return (
-        <div>
-            <li className="nav-item">
-                <Link to={props.link} className="nav-link">{props.name}</Link>
-            </li>
-        </div>
-    );
+  return (
+    <div>
+      <li className='nav-item'>
+        <Link to={props.link} className='nav-link'>
+          {props.name}
+        </Link>
+      </li>
+    </div>
+  );
 }
 
 export default Navlinks;
